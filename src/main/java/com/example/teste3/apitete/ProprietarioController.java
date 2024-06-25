61512c6b860b01abd716383946bf9298d714edb3
Pedro Carrara <pepe.carrara@gmail.com>
2024-06-25 17:42:58 -0300

    add total

--- src/main/java/com/example/teste3/apitete/ProprietarioController.java
+++ src/main/java/com/example/teste3/apitete/ProprietarioController.java
@@ -23,7 +23,7 @@ import java.util.Optional;
 @RequestMapping(("/proprietarios"))
 public class ProprietarioController {
 
-
+    private Double valorTotal;
     private ProprietarioRepository proprietarioRepository;
 
     @GetMapping
